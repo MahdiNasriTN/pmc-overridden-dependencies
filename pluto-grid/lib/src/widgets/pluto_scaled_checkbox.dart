1033b22c015950d9b726e9952253361ca4781aa5
MahdiNasriTN <mahdi.nasri123@gmail.com>
2025-10-09 12:00:29 +0100

    new updates

--- pluto-grid/lib/src/widgets/pluto_scaled_checkbox.dart
+++ pluto-grid/lib/src/widgets/pluto_scaled_checkbox.dart
@@ -2,17 +2,11 @@ import 'package:flutter/material.dart';
 
 class PlutoScaledCheckbox extends StatelessWidget {
   final bool? value;
-
   final Function(bool? changed) handleOnChanged;
-
   final bool tristate;
-
   final double scale;
-
   final Color unselectedColor;
-
   final Color? activeColor;
-
   final Color checkColor;
 
   const PlutoScaledCheckbox({
@@ -21,7 +15,7 @@ class PlutoScaledCheckbox extends StatelessWidget {
     required this.handleOnChanged,
     this.tristate = false,
     this.scale = 1.0,
-    this.unselectedColor = Colors.black26,
+    this.unselectedColor = const Color(0xFFD0D0D0), // lighter gray border
     this.activeColor = Colors.lightBlue,
     this.checkColor = const Color(0xFFDCF5FF),
   }) : super(key: key);
@@ -38,8 +32,23 @@ class PlutoScaledCheckbox extends StatelessWidget {
           value: value,
           tristate: tristate,
           onChanged: handleOnChanged,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(3.0), // smaller radius, closer to your example
+          ),
+          fillColor: MaterialStateProperty.resolveWith<Color?>((states) {
+            if (states.contains(MaterialState.selected)) {
+              return activeColor;
+            }
+            return Colors.white;
+          }),
+          side: BorderSide(
+            color: unselectedColor,
+            width: 1.0, // thinner border
+          ),
           activeColor: value == null ? unselectedColor : activeColor,
           checkColor: checkColor,
+          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          visualDensity: VisualDensity.compact,
         ),
       ),
     );
